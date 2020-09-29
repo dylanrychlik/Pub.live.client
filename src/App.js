833032b07c76f9580c030477b077867f74253914
dylanrychlik <dylanrychlik@gmail.com>
2020-09-29 16:21:24 -0400

    Update App.js
    wrong file uploaded

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import TransformText from "./TransformText";
+//import TransformText from "./TransformText";
 import Link from '@material-ui/core/Link';
 import Popover from '@material-ui/core/Popover';
 //import * as Yup from 'yup';
@@ -16,66 +16,62 @@ import Popover from '@material-ui/core/Popover';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import axios from "axios";
 
-
-
-class App extends Component {
-  constructor(props){
-        super();
-        this.props = props;
-        this.companyFetcher = props.companyFetcher;
-        this.state = {
-            authenticated : null
-        }
+  class App extends React.Component {
+   state = {
+    //email:' ',
+    //password : ' '
+  }
+constructor(props) {
+        super(props);
     }
 
-    handleSubmit = (e) =>{
-        e.preventDefault();
-        const data = new FormData(e.target);
-        this.companyFetcher.sendLoginData(this.stringifyFormData(data))
-        .then(result=>{
-            alert(result);
-            this.handleLoginStatus(result);
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    }
 
-    handleLoginStatus = (status) =>{
-        const {history} = this.props;
-        switch (status) {
-            case 200:
-                alert("OK")
-                this.props.authenticate();
-                history.push('/home/dashboard');
-                break;
-            case 401:
-                alert("AUTH FAIL")
-                this.setState({
-                    authenticated : false
-                })
-                break;
-            default:
-                alert("DEFAULT")
-                this.setState({
-                    authenticated : "ERR"
-                })
-                break;
-        }
-    }
+    async login() {
+      console.log('test');
 
-    stringifyFormData = (fd) => {
-        const data = {};
-          for (let key of fd.keys()) {
-            data[key] = fd.get(key);
-        }
-        return JSON.stringify(data, null, 2);
-      }
+ fetch('http://localhost:8080/')
+   .then(res => res.json())
+      .then(json => this.setState({ data: json }));
+    console.log('test 2');
+// const json = await response.json();
+  //console.log('json:',data);
+    //this.setState({ data: json });
+    // body data type must match "Content-Type" header
+   
+   
+  
+  
+ // return response.json(); // parses JSON response into native JavaScript objects */
+    } 
 
- 
+    componentDidMount(){
+     this.login();
+}
+
+ async postData(url = '')  {
+  console.log('VICTORY 37!!!');
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+body: {
+    "Username": this.email.value,
+     "Password": this.password.value
+   }
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+  }
+   handleSubmit() {
+  console.log('VICTORY 37!!!');
+
+this.postData('http://localhost:8080/login', data = { email: 'dylanrychlik@gmail.com',password: 'password' })
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
+ }
   render() {
-    return (
+      return (
       <div className="App">
       
         <header className="App-header">
@@ -84,14 +80,7 @@ class App extends Component {
            </Typography>
           <div className="Login" width="120px">
          
-            <Label
-              variant="standard"
-              placeholder="Username"
-              margin="normal"
-              required
-              onChange={this.setUsername}
-              value={this.state.username}
-            />
+           
             <Typography variant="h2" gutterBottom
          style={{ color: "black" }}> Sign in
            </Typography>
@@ -124,13 +113,13 @@ class App extends Component {
             />
 
             <div className="Button">
-             <Button style={{ background: "Orange",  borderstyle: "solid", color: "Black" }}>Sign-in</Button>
+             <Button style={{ background: "Orange",  borderstyle: "solid", color: "Black" }}     onClick={this.handleSubmit} >Sign-in</Button>
             </div>
 
 
 
             <div className="Button">
-             <Button style={{ background: "Silver", color: "Black" }}>New to Pubdata.live? Sign in here</Button>
+             <Button style={{ background: "Silver", color: "Black" }}>New to Pubdata.live? Sign in here </Button>
             </div>
           </div>
          
@@ -138,6 +127,7 @@ class App extends Component {
       </div>
     );
   }
-}
+
+  }
 
 export default App;
